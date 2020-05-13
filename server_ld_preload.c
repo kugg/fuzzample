c3c651f335163c3146ca8f64317d22d9536165a6
Pasi Saarinen <pasi.saarinen@f-secure.com>
2020-05-13 02:34:59 -0700

    Fix indentation

--- server_ld_preload.c
+++ server_ld_preload.c
@@ -1,5 +1,4 @@
 #define _GNU_SOURCE
-
 // gcc -fPIC -shared server_ld_preload.c -o server_ld_preload.so -ldl
 
 #include <arpa/inet.h>
@@ -28,39 +27,39 @@ static int listener_fd = -1;
 
 __attribute__((constructor)) void prep_orig()
 {
-	original_close = dlsym(RTLD_NEXT, "close");
+    original_close = dlsym(RTLD_NEXT, "close");
     original_select = dlsym(RTLD_NEXT, "select");
     original_recv = dlsym(RTLD_NEXT, "recv");
     original_send = dlsym(RTLD_NEXT, "send");
 }
 
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
-	return MAGIC_SOCK_FD;
+    return MAGIC_SOCK_FD;
 }
 int bind(int sockfd, const struct sockaddr *addr,
                 socklen_t addrlen) {
-	listener_fd = sockfd;
-	return 0;
+    listener_fd = sockfd;
+    return 0;
 }
 /*
 // Do something like this for clients
 int socket(int domain, int type, int protocol) {
-	return MAGIC_SOCK_FD;
+    return MAGIC_SOCK_FD;
 }
 int connect(int sockfd, const struct sockaddr *addr,
                    socklen_t addrlen) {
-	input_data_fd = STDIN_FILENO;
-	return 0;
+    input_data_fd = STDIN_FILENO;
+    return 0;
 }*/
 
 int select(int nfds, fd_set *readfds, fd_set *writefds,
                   fd_set *exceptfds, struct timeval *timeout) {
-	printf("select: magic socket\n");				  
+    printf("select: magic socket\n");                 
     if (readfds != NULL) {
         int is_socket = nfds > MAGIC_SOCK_FD && FD_ISSET(MAGIC_SOCK_FD, readfds);
         int is_listener = nfds > listener_fd && FD_ISSET(listener_fd, readfds);
         if (is_socket || is_listener) {
-			if (writefds != NULL) {
+            if (writefds != NULL) {
                 FD_ZERO(writefds);
             }
             if (exceptfds != NULL) {
@@ -68,12 +67,12 @@ int select(int nfds, fd_set *readfds, fd_set *writefds,
             }
             FD_ZERO(readfds);
             if (is_socket) {
-				FD_SET(MAGIC_SOCK_FD,readfds);
-			} else {
-				FD_SET(listener_fd,readfds);
-			}
+                FD_SET(MAGIC_SOCK_FD,readfds);
+            } else {
+                FD_SET(listener_fd,readfds);
+            }
             return 1;
-		}
+        }
    
     }
     
@@ -87,9 +86,9 @@ int select(int nfds, fd_set *readfds, fd_set *writefds,
                 FD_ZERO(exceptfds);
             }
             FD_ZERO(writefds);
-			
-			FD_SET(MAGIC_SOCK_FD,writefds);
-			return 1;
+            
+            FD_SET(MAGIC_SOCK_FD,writefds);
+            return 1;
         }
     }
     
@@ -101,13 +100,13 @@ int select(int nfds, fd_set *readfds, fd_set *writefds,
 ssize_t recv(int sockfd, void *buf, size_t len, int flags) {
     printf("recv: fd: %d\n",sockfd);
     if (sockfd == MAGIC_SOCK_FD) {
-		// TODO: Handle special flags such as PEEK
+        // TODO: Handle special flags such as PEEK
         return read(STDIN_FILENO, buf,len);
     }
     return original_recv(sockfd, buf, len, flags);
 }
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
-	printf("send: fd: %d\n",sockfd);
+    printf("send: fd: %d\n",sockfd);
     if (sockfd == MAGIC_SOCK_FD) {
         return write(STDOUT_FILENO, buf,len);
     }
@@ -116,8 +115,8 @@ ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
 
 int close(int fd) {
     if (fd == MAGIC_SOCK_FD) {
-		exit(0);
-		return 0;
+        exit(0);
+        return 0;
     }
     return original_close(fd);
 }
